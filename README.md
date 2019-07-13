a5a604bb405a7cbe9c30c6661b1811d085d3af15
Jose L Crisostomo <jl.cs606@gmail.com>
2019-07-13 12:39:25 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Flights!
+The app allow you to get the best prices from a famous API 
+
+# API KEY
+In order to run the app the following variables must be created in the `gradle.properties` located in `$USER/.gradle`
+* `SKY_API_KEY = your_api_key`
+* `SKY_URL_BASE_ENDPOINT = "http://partners.api.skyscanner.net/apiservices/"`
+
+
+
+
---
+++ app/src/main/java/com/simplekjl/flights/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.simplekjl.flights
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * Comment added since the README.md file might be ignored!
+ *
+ *
+ * Flights!
+ * The app allow you to get the best prices from a famous API
+ *
+ * API KEY
+ * In order to run the app the following variables must be created in the `gradle.properties` located in `$USER/.gradle`
+ * `SKY_API_KEY = your_api_key`
+ * `SKY_URL_BASE_ENDPOINT = "http://partners.api.skyscanner.net/apiservices/"`
+ *
+ *
+ */
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
